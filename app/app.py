d2235174ce56ffbfd39ad911baac5b857b1c8c0e
EC2 Default User <ec2-user@ip-172-31-2-34.ap-south-1.compute.internal>
2025-07-31 08:46:43 +0000

    Fetched Data

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Ajith Kumar!"
+    return "Hello from Ajith Kumar-Server-1!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
